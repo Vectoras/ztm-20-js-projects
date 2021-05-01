2116c51853fca84d4b293f39bc0b0a40b8d278a1
Ionut Boghean <ionut.boghean.conturi@gmail.com>
2021-05-01 16:36:44 +0200

    project finished

--- 5. (7) Light & Dark Mode/script.js
+++ 5. (7) Light & Dark Mode/script.js
@@ -1,1 +1,33 @@
-console.log("it works");
+const changeThemeButton = document.getElementById("color-switch-checkbox");
+const themeModeDisplayArea = document.getElementById("color-mode");
+const svgColor = document.getElementsByClassName("svg-main-color");
+
+// functions
+let updateColorOnSvg = (color) => {
+  for (let i = 0; i < svgColor.length; i++) {
+    console.log(svgColor[i]);
+    svgColor[i].setAttribute("fill", color);
+  }
+};
+
+let changeTheme = (theme) => {
+  if (theme === "dark") {
+    document.documentElement.setAttribute("data-color-mode", "dark");
+    themeModeDisplayArea.textContent = "Dark Mode";
+    console.log(svgColor);
+    updateColorOnSvg("#009c99");
+  } else {
+    document.documentElement.removeAttribute("data-color-mode");
+    themeModeDisplayArea.textContent = "Light Mode";
+    updateColorOnSvg("#ff6366");
+  }
+};
+
+// events
+changeThemeButton.addEventListener("change", () => {
+  if (changeThemeButton.checked === true) {
+    changeTheme("dark");
+  } else {
+    changeTheme("light");
+  }
+});
