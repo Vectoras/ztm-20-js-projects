97fff2754ce64a8bcad8b7513e2f77a3ac8eb78b
Ionut Boghean <ionut.boghean.conturi@gmail.com>
2021-05-24 09:27:08 +0200

    Optimization based on Andrei review of code

--- 16. (18) Drag n' Drop/script.js
+++ 16. (18) Drag n' Drop/script.js
@@ -140,23 +140,10 @@ function hideInputBox(column) {
 
 //  Allows arrays to reflect Drag and Drop items
 function rebuildArrays() {
-  backlogListArray = [];
-  progressListArray = [];
-  completeListArray = [];
-  onHoldListArray = [];
-
-  for (let i = 0; i < backlogList.children.length; i++) {
-    backlogListArray.push(backlogList.children[i].textContent);
-  }
-  for (let i = 0; i < progressList.children.length; i++) {
-    progressListArray.push(progressList.children[i].textContent);
-  }
-  for (let i = 0; i < completeList.children.length; i++) {
-    completeListArray.push(completeList.children[i].textContent);
-  }
-  for (let i = 0; i < onHoldList.children.length; i++) {
-    onHoldListArray.push(onHoldList.children[i].textContent);
-  }
+  backlogListArray = Array.from(backlogList.children).map((currentChild) => currentChild.textContent);
+  progressListArray = Array.from(progressList.children).map((currentChild) => currentChild.textContent);
+  completeListArray = Array.from(completeList.children).map((currentChild) => currentChild.textContent);
+  onHoldListArray = Array.from(onHoldList.children).map((currentChild) => currentChild.textContent);
   updateDOM();
 }
 
